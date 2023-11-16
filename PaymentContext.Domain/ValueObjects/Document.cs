d8568f3b29292ab2465af6023753ec95a8f82578
phelippemichel <phelippe.michel@gmail.com>
2023-11-16 17:28:57 -0300

    feat: add notification and validate to class

--- PaymentContext.Domain/ValueObjects/Document.cs
+++ PaymentContext.Domain/ValueObjects/Document.cs
@@ -1,16 +1,33 @@
 using PaymentContext.Domain.Enums;
+using PaymentContext.Shared.ValueObjects;
+using Flunt.Validations;
 
 namespace PaymentContext.Domain.ValueObjects
 {
-    public class Document
+    public class Document : ValueObject
     {
         public Document(string number, EDocumentType type)
         {
             Number = number;
             Type = type;
+
+            AddNotifications(new Contract<Document>()
+                .Requires()
+                .IsTrue(Validate(), "Document.Number", "Documento inválido")
+            );
         }
 
         public string Number { get; set; }
         public EDocumentType Type { get; private set; }
+
+        private bool Validate(){
+            if(Type == EDocumentType.CNPJ && Number.Length == 14)
+                return true;
+            
+            if (Type == EDocumentType.CPF && Number.Length == 11)
+                return true;
+
+            return false;
+        }
     }
 }

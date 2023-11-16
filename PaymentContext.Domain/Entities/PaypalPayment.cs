748d2cedbab6b45b81d5d2bca953a70e14c12e92
phelippemichel <phelippe.michel@gmail.com>
2023-11-16 17:29:43 -0300

    refactor: struct code

--- PaymentContext.Domain/Entities/PaypalPayment.cs
+++ PaymentContext.Domain/Entities/PaypalPayment.cs
@@ -1,12 +1,32 @@
+using System;
+using PaymentContext.Domain.ValueObjects;
+
 namespace PaymentContext.Domain.Entities
 {
-    public class PaypalPayment : Payment
+    public class PayPalPayment : Payment
     {
-        public TransactionCode(string TransactionCode)
-        :base(DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, Document document, Address address, Email email)
+        public PayPalPayment(
+            string transactionCode, 
+            DateTime paidDate, 
+            DateTime expireDate, 
+            decimal total, 
+            decimal totalPaid, 
+            string payer, 
+            Document document, 
+            Address address, 
+            Email email) : base(
+                paidDate, 
+                expireDate, 
+                total, 
+                totalPaid, 
+                payer, 
+                document, 
+                address, 
+                email)
         {
             TransactionCode = transactionCode;
         }
+
         public string TransactionCode { get; private set; }
     }
 }
